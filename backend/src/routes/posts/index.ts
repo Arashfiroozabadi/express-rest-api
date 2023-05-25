96e301e8be5cbc5be7839eddc6abcf92cc0e9a85
firoozabadi <arashfiroozabadii@gmail.com>
2023-05-26 01:45:39 +0330

    add endpoint :: create new post

--- backend/src/routes/posts/index.ts
+++ backend/src/routes/posts/index.ts
@@ -1,9 +1,30 @@
 import { Router } from 'express';
+import { ObjectId } from 'bson';
 import * as yup from 'yup';
 
 import PostModel from '../../model/Post';
-
+import { IPost } from '../../interfaces';
 const routes = Router();
+
+async function checkPostData(post: IPost): Promise<any> {
+    const validationSchema = yup.object({
+        title: yup.string().required('title is required'),
+        description: yup.string().required('description is required'),
+        abstract: yup.string().required('abstract is required'),
+        author: yup.string().test('valid-object-id', 'Invalid Object ID', (value) => {
+            return ObjectId.isValid(value);
+        })
+    });
+    return validationSchema.validate(post)
+        .then(result => result)
+        .catch(err => {
+            return {
+                err: true,
+                msg: err.errors.join(', ')
+            };
+        });
+}
+
 /**
  * Retrieve all posts from the database
  */
@@ -14,7 +35,15 @@ routes.get('/', async (req, res) => {
 
 
 routes.post('/', async (req, res) => {
+    const { body } = req;
 
+    const validateBody = await checkPostData(body);
+    // send error response
+    if (validateBody.err) return res.status(400).send({ msg: validateBody.msg });
+
+    const newPost = new PostModel({ body });
+
+    return res.status(201).send({ _id: newPost._id });
 });
 
 
