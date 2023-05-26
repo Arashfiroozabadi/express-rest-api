66eb68a22b9cf817610527581f575c8135e005bb
firoozabadi <arashfiroozabadii@gmail.com>
2023-05-27 00:53:53 +0330

    add lib :: add handleErrors.ts for send unexpected errors or mongodb errors like duplicate unique keys

---
+++ backend/src/lib/handleErrors.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+export default function(err: any, req: Request, res: Response) {
+    if ('code' in err) {
+        const code = err.code;
+        return res.status(400).send({
+            msg: req.t(code),
+            field: err.keyValue
+        });
+    } else {
+        return res.status(500).send(err);
+    }
+}
