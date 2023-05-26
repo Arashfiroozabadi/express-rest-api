4aafcd565ded7607ff2750d2885fbf671b3d57b9
firoozabadi <arashfiroozabadii@gmail.com>
2023-05-27 01:04:06 +0330

    update model :: add new keys and generateAuthToken method for generate and save jwt token

--- backend/src/model/User.ts
+++ backend/src/model/User.ts
@@ -1,23 +1,41 @@
 import mongoose from 'mongoose';
 import { IUser } from '../interfaces';
+import jwt from 'jsonwebtoken';
 
+const JWT_KEY = process.env.JWT_KEY as string;
 const Schema = mongoose.Schema;
 
 const user = new Schema<IUser>(
     {
         name: {
-            type: String,
-            required: true
-        },
-        email: {
             type: String
         },
+        email: {
+            type: String,
+            unique: true
+        },
         phone: {
+            type: String,
+            unique: true
+        },
+        password: {
+            type: String
+        },
+        token: {
             type: String
         }
     },
     { timestamps: true }
 );
+
+user.methods.generateAuthToken = async function() {
+    const expiresIn = '2d';
+    const user = this;
+    const token = jwt.sign({ _id: user._id }, JWT_KEY, { expiresIn });
+    user.token = token;
+    await user.save();
+    return token;
+};
 
 const UserModel = mongoose.model<IUser>('User', user);
 
