2a73d780984537f4be64a4ced79ca60a6ca30e7c
firoozabadi <arashfiroozabadii@gmail.com>
2023-05-27 16:14:44 +0330

    add feature :: refresh token : working on progress

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
-
+import moment from 'moment';
 import UserModel from '../model/User';
 import { notAuth } from '../lib/i18nResources';
+import handleErrors from '../lib/handleErrors';
 
 const JWT_KEY = process.env.JWT_KEY as string;
 
@@ -12,24 +13,31 @@ const auth = async (req: Request, res: Response, next: NextFunction) => {
             const token = req.header('Authorization')?.replace('Bearer ', '');
             if (token && token !== 'null') {
                 const decoded = jwt.verify(token, JWT_KEY) as { _id: string, exp: number };
-                console.log(decoded.exp);
+
+                // const newDate = moment('2023-05-26T16:05:58+03:30');
+                // const expireDate = moment(decoded.exp * 1000);
+                // if (expireDate.diff(newDate, 'hours') < 8) {
+                //
+                // }
+
                 let user = await UserModel.findOne({ _id: decoded._id, token });
 
                 // send error response
-                if (!user) return res.status(401).send({ error: req.t(notAuth) });
-                console.log(user.token);
-                user.generateAuthToken();
-                console.log(user.token);
+                if (!user) return res.status(401).send({ msg: req.t(notAuth) });
 
                 req.user = user;
                 next();
             }
         } else {
-            return res.status(401).send({ error: req.t(notAuth) });
+            return res.status(401).send({ msg: req.t(notAuth) });
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ error });
+    } catch (err: any) {
+        if (err.name === 'TokenExpiredError') {
+            return res.status(401).send({ msg: req.t('TokenExpiredError') });
+        } else {
+            handleErrors(err, req, res);
+        }
+
     }
 };
 
