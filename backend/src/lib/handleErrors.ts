117c34eec36887cf29ff08e1bda4c1359735d875
firoozabadi <arashfiroozabadii@gmail.com>
2023-05-27 22:48:25 +0330

    log errors

--- backend/src/lib/handleErrors.ts
+++ backend/src/lib/handleErrors.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 export default function(err: any, req: Request, res: Response) {
+    console.error(err);
     if ('code' in err) {
         const code = err.code;
         return res.status(400).send({
